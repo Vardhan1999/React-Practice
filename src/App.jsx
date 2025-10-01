8b8eaefb9a8d0ff1fd78e5cc9b68be367197bb92
Vardhan Jain <vardhanjaindit@gmail.com>
2025-10-02 00:08:40 +0530

    Todos Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
 import { useState } from "react";
 
 export default function App() {
-  const [count, setCount] = useState(0)
+  const [text, setText] = useState("");
+  const [todo, setTodo] = useState([]);
+
+  function addTodos() {
+    if (!text.trim()) return;
+    setTodo([...todo, { id: crypto.randomUUID(), text, completed: false }]);
+    setText("");
+  }
+
+  function toggleTodo(id) {
+    setTodo(todo.map(todo => todo.id === id ? { ...todo, completed: !todo.completed } : todo))
+  }
+
+  function deleteTodo(id) {
+    setTodo(
+      todo.filter(todo => todo.id !== id))
+  }
 
   return (
     <div>
-      <h2>Counter:{count}</h2>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-      <button onClick={() => setCount(count - 1)} disabled={count === 0}>Decrement</button>
-      <button onClick={() => setCount(0)}>Reset</button>
+      <h2>Todo List</h2>
+      <input value={text} onChange={e => setText(e.target.value)} placeholder="Enter Todos..." />
+      <button onClick={addTodos}>Add</button>
+      <ul>
+        {todo.map(todo => (<li key={todo.id}>
+          <input type="checkbox" checked={todo.completed} onChange={() => toggleTodo(todo.id)} />
+          {todo.text}
+          <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+        </li>))}
+      </ul>
     </div>
   )
 }
