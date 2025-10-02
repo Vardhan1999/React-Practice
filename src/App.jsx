3c383a79024dc665ec3cba9d169cb13419bc6ec7
Vardhan Jain <vardhanjaindit@gmail.com>
2025-10-03 00:06:33 +0530

    Search App

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,17 @@
 import { useState } from "react";
 
 export default function App() {
-  const [text, setText] = useState("");
-  const [todo, setTodo] = useState([]);
-
-  function addTodos() {
-    if (!text.trim()) return;
-    setTodo([...todo, { id: crypto.randomUUID(), text, completed: false }]);
-    setText("");
-  }
-
-  function toggleTodo(id) {
-    setTodo(todo.map(todo => todo.id === id ? { ...todo, completed: !todo.completed } : todo))
-  }
-
-  function deleteTodo(id) {
-    setTodo(
-      todo.filter(todo => todo.id !== id))
-  }
+  const [query, setQuery] = useState("");
+  const items = ["Apple", "Banana", "Cherry", "Date", "Onion"];
+  const filtered = items.filter(item => item.toLowerCase().includes(query.toLowerCase()));
 
   return (
     <div>
-      <h2>Todo List</h2>
-      <input value={text} onChange={e => setText(e.target.value)} placeholder="Enter Todos..." />
-      <button onClick={addTodos}>Add</button>
+      <input placeholder="Search..." onChange={(e) => setQuery(e.target.value)} value={query} />
       <ul>
-        {todo.map(todo => (<li key={todo.id}>
-          <input type="checkbox" checked={todo.completed} onChange={() => toggleTodo(todo.id)} />
-          {todo.text}
-          <button onClick={() => deleteTodo(todo.id)}>Delete</button>
-        </li>))}
+        {
+          filtered.map((item, idx) => (<li key={idx}>{item}</li>))
+        }
       </ul>
     </div>
   )
