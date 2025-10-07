9328a3d8575dccb8fc2fcc4ee255040625888174
Vardhan Jain <vardhanjaindit@gmail.com>
2025-10-07 19:02:52 +0530

    Timer 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function App() {
-  const [open, setOpen] = useState(false);
+  const [seconds, setSeconds] = useState(0);
 
-  return (
-    <div>
-      <button onClick={() => setOpen(true)}>Open Modal</button>
-      {open && (
-        <div>
-          <div>Modal Content</div>
-          <button onClick={() => setOpen(false)}>Close</button>
-        </div>
-      )}
-    </div>
-  )
+  useEffect(() => {
+    const interval = setInterval(() => setSeconds(s => s + 1), 1000);
+
+    return () => clearInterval(interval);
+  }, [])
+
+  return <h2>Timer: {seconds}s</h2>
 }
